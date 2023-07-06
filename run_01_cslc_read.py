244d8ae063c9c24858561083e81df1af61e0db3a
Jinwoo Kim (jinwook@smu.edu) <85753525+smuinsar@users.noreply.github.com>
2023-07-05 20:19:00 -0500

    Parallel streaming

--- run_01_cslc_read.py
+++ run_01_cslc_read.py
@@ -2,7 +2,79 @@
 import argparse
 import datetime as dt
 import os, time
+import numpy as np
 from src.RLE_utils import hdf_stream, hdf_read, convert_to_slcvrt
+from multiprocessing import Process
+
+def fetch_cslc(day):
+    outSLC = slc_dir + '/' + day + '.slc'
+    outSLCvrt = outSLC + '.vrt'
+
+    if os.path.isfile(outSLC) and (os.path.isfile(outSLCvrt)):
+        print(f'{day}.slc exist. \n')
+    else:
+        path_h5 = f'{data_dir}/{burst_id}/{day}/{burst_id}_{day}.h5'   #path to COMPASS CSLC h5 file
+        cslc_source = data_dir[0:2]
+        if (cslc_source == 's3'):
+            xcoor, ycoor, dx, dy, epsg, slc, date = hdf_stream(path_h5)   
+            convert_to_slcvrt(xcoor, ycoor, dx, dy, epsg, slc, date, slc_dir)   #generating slc with vrt
+        else:
+            xcoor, ycoor, dx, dy, epsg, slc, date = hdf_read(path_h5)
+            convert_to_slcvrt(xcoor, ycoor, dx, dy, epsg, slc, date, slc_dir)
+
+def multi_stream(days,n_multi):
+
+    n_dates = len(days)    #number of all dates
+    n_group = int(np.ceil(n_dates/n_multi))   #number of groups for multiprocessing
+
+    st_time = time.time()
+    print('Streaming started')
+
+    for i in range(n_group):
+         _i_min = i*n_multi
+         _i_max = min(_i_min+n_multi,n_dates)
+
+         _days = days[_i_min:_i_max] 
+
+         print(f'{_days} to be downloaded')
+
+         processes = [Process(target=fetch_cslc, args=(day,)) for day in _days]
+
+         for process in processes:
+             process.start()
+         for process in processes:  
+             process.join()
+
+         processes = None
+
+    end_time = time.time()
+    time_taken = (end_time - st_time)/60.
+    print(f'{time_taken} min taken for multi-streaming')
+
+def check_missing(days):
+    #checking if all files are streamed correctly
+    missing_dates = []
+
+    for _ in days:
+        outSLC = slc_dir + '/' + _ + '.slc'
+        outSLCvrt = outSLC + '.vrt'
+
+        if os.path.isfile(outSLC) and os.path.isfile(outSLCvrt):
+            pass
+        else:
+            missing_dates.append(_)
+
+    n_miss = len(missing_dates)    
+ 
+    if not missing_dates:
+        pass
+    else:
+        print(f'missing dates: {missing_dates}')
+        print('Warning: missing dates are streamed...')
+        multi_stream(missing_dates,n_multi)
+        print('finished...') 
+
+    return n_miss
 
 def createParser(iargs = None):
     '''Commandline input parser'''
@@ -15,11 +87,14 @@ def createParser(iargs = None):
                          required=True, type=str, help='text file with 1-column dates (YYYYMMDD) to be processed')
     parser.add_argument("--slc_dir", dest="slc_dir",
             default='SLCDIR', type=str, help='slc directory (default: SLCDIR)')
+    parser.add_argument("--n_multi", dest="n_multi",
+            default=4, type=int, help='number of multi streaming/downloading (default: 4)')
 
     return parser.parse_args(args=iargs)
 
 def run(inps):
 
+    global data_dir, burst_id, slc_dir, n_multi
     data_dir = inps.inputs
     burst_id = inps.bid
 
@@ -27,30 +102,28 @@ def run(inps):
     f = open(inps.dfile)
     datels = f.read().splitlines()
 
-    st_time = time.time()
+    n_multi = inps.n_multi
 
-    #creating slc inputs from COMPASS hdf file
-    for day in datels:
-        outSLC = slc_dir + '/' + day + '.slc'
-        outSLCvrt = outSLC + '.vrt'
+    print(f'input location: {data_dir}')
+    print(f'burst ID: {burst_id}')
+    print(f'output SLC directory {slc_dir}')
+    print(f'number of multi processing {n_multi}\n')
+  
+    multi_stream(datels,n_multi)  #streaming via multiprocessing
 
-        if os.path.isfile(outSLC) and (os.path.isfile(outSLCvrt)):
-            print(f'{day}.slc exist. \n')
-        else:
-            path_h5 = f'{data_dir}/{burst_id}/{day}/{burst_id}_{day}.h5'   #path to COMPASS CSLC h5 file
+    #downloading missed dates
+    n_iter = 100
+    
+    for ii in range(n_iter):
+        n_miss = check_missing(datels)
+  
+        if (n_miss == 0):
+            break
 
-            cslc_source = data_dir[0:2]
-
-            if (cslc_source == 's3'):
-                xcoor, ycoor, dx, dy, epsg, slc, date = hdf_stream(path_h5)   
-                convert_to_slcvrt(xcoor, ycoor, dx, dy, epsg, slc, date, slc_dir)   #generating slc with vrt 
-            else:
-                xcoor, ycoor, dx, dy, epsg, slc, date = hdf_read(path_h5)  
-                convert_to_slcvrt(xcoor, ycoor, dx, dy, epsg, slc, date, slc_dir)   #generating slc with vrt 
-
-    end_time = time.time()
-    time_taken = (end_time - st_time)/60.
-    print(f'{time_taken} min taken for preparing CSLC before pycuampcor')
+    if (n_miss == 0):
+        print('all files are successfully streamed')
+    else:
+        print('still missing dates to be downloaded')
 
 if __name__ == '__main__':
     # load arguments from command line
